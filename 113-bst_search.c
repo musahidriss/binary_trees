14420b4645ab000575751f5546c80e35394461bc
musahidriss <113646928+musahidriss@users.noreply.github.com>
2023-03-30 14:58:12 +0000

    Update 113-bst_search.c

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert -
- *
+ * bst_insert - searches for a value in a Binary Search Tree
+ * tree: is a pointer to the root node of the BST to search
+ * value: is the value to search in the tree
  * Return:
 */
 
