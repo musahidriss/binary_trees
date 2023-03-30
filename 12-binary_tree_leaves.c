922cf921db950a7da104b7e89fbe2c0c56faa0f7
musahidriss <113646928+musahidriss@users.noreply.github.com>
2023-03-30 14:21:07 +0000

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
 * binary_tree_leaves - leaves a binary tree
 * @tree: the binary tree
@@ -8,10 +7,11 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
-  if (tree->left == NULL && tree->right == NULL)
-    return (1);
-  else
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
